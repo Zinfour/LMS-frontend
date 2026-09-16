a17a8de8bb7d48b163e4309b18d9e8a04a88f295
Dragos Cuciureanu <dragos@triggybet.com>
2026-09-16 09:41:04 +0200

    fixed NaN problem

--- src/screens/MyCourse/Module.tsx
+++ src/screens/MyCourse/Module.tsx
@@ -93,7 +93,7 @@ export default function CourseModule() {
           </CardHeader>
           <CardContent>
             <div className="flex items-baseline gap-2">
-              <h3 className="text-4xl font-bold">{module.moduleProgress}%</h3>
+              <h3 className="text-4xl font-bold">{module.moduleProgress || 0}%</h3>
               <p className="text-muted-foreground font-light">
                 {module.numberOfCompletedActivities} / {module.numberOfActivities} activities
               </p>
